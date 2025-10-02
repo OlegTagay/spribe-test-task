433dbe920631d73d7d7a4bfdfcc9b2d4944aeabe
otagay <olegtagay@gmail.com>
2025-10-02 09:43:17 +0200

    patch tests added;

--- src/test/java/com/spribe/tests/player/VerifyUpdate.java
+++ src/test/java/com/spribe/tests/player/VerifyUpdate.java
@@ -2,6 +2,7 @@ package com.spribe.tests.player;
 
 import com.spribe.project.config.ConfigManager;
 import com.spribe.project.enums.Editor;
+import com.spribe.project.enums.Gender;
 import com.spribe.project.models.common.HttpStatus;
 import com.spribe.project.models.response.get.PlayerItemResponse;
 import com.spribe.project.models.response.update.PlayerUpdateResponse;
@@ -11,6 +12,8 @@ import com.spribe.tests.base.BaseTest;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.testng.Tag;
+import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
@@ -27,21 +30,20 @@ public class VerifyUpdate extends BaseTest {
         PlayerQueryService playerQueryService = new PlayerQueryService();
         Optional<PlayerItemResponse> playerItemResponse = playerQueryService.getById(randomPlayerResponse.get().getId());
 
-        System.out.println("-".repeat(10));
-        System.out.println(playerItemResponse.get());
-        System.out.println("-".repeat(10));
+        String expectedField = "atf-updated";
 
-        playerItemResponse.get().setScreenName("atf-updated");
+        playerItemResponse.get().setScreenName(expectedField);
 
         PlayerUpdateResponse response =
                 given()
                         .baseUri(ConfigManager.getBaseUri())
                         .when()
+                        .header("Content-Type", ContentType.JSON)
                         .pathParam("editor", Editor.SUPERVISOR)
                         .pathParam("id", playerItemResponse.get().getId())
                         .body(playerItemResponse.get())
                         .log().all()
-                        .patch("/player/create/{editor}/{id}")
+                        .patch("/player/update/{editor}/{id}")
                         .then()
                         .log().all()
                         .statusCode(HttpStatus.OK.code())
@@ -49,9 +51,60 @@ public class VerifyUpdate extends BaseTest {
                         .as(PlayerUpdateResponse.class);
 
 
-        System.out.println("-".repeat(10));
-        System.out.println(playerItemResponse.get());
-        System.out.println("-".repeat(10));
-//        PlayerAssertions.assertMatches(request, response);
+        Assert.assertEquals(playerItemResponse.get().getScreenName(), expectedField);
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Tag("Smoke")
+    @Tag("Update")
+    @Test(description = "Update user screen name should return 200")
+    public void createUserGender_shouldReturn200() {
+        PlayerQueryService playerQueryService = new PlayerQueryService();
+        Optional<PlayerItemResponse> playerItemResponse = playerQueryService.getById(randomPlayerResponse.get().getId());
+
+        playerItemResponse.get().setGender(Gender.FEMALE.toString());
+
+        PlayerUpdateResponse response =
+                given()
+                        .baseUri(ConfigManager.getBaseUri())
+                        .when()
+                        .header("Content-Type", ContentType.JSON)
+                        .pathParam("editor", Editor.SUPERVISOR)
+                        .pathParam("id", playerItemResponse.get().getId())
+                        .body(playerItemResponse.get())
+                        .log().all()
+                        .patch("/player/update/{editor}/{id}")
+                        .then()
+                        .log().all()
+                        .statusCode(HttpStatus.OK.code())
+                        .extract()
+                        .as(PlayerUpdateResponse.class);
+
+
+        Assert.assertEquals(playerItemResponse.get().getGender(), Gender.FEMALE.toString());
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Tag("Smoke")
+    @Tag("Update")
+    @Test(description = "Update user screen name should return 403")
+    public void createUserGenderWithInvalidEditor_shouldReturn403() {
+        PlayerQueryService playerQueryService = new PlayerQueryService();
+        Optional<PlayerItemResponse> playerItemResponse = playerQueryService.getById(randomPlayerResponse.get().getId());
+
+        playerItemResponse.get().setGender(Gender.FEMALE.toString());
+
+        given()
+                .baseUri(ConfigManager.getBaseUri())
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .pathParam("editor", Editor.INVALID)
+                .pathParam("id", playerItemResponse.get().getId())
+                .body(playerItemResponse.get())
+                .log().all()
+                .patch("/player/update/{editor}/{id}")
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.FORBIDDEN.code());
     }
 }
